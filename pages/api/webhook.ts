95a673e7c616a968c20632311a60b9e514feaabb
Pavel Marchenko <bblrlo@gmail.com>
2023-05-26 21:23:39 +0300

    upd

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -22,7 +22,7 @@ export default async function webhook(request: NextApiRequest, response: NextApi
     }catch(err){
         const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
         const sendMessage_URL = `${BASE_URL}/sendMessage`;
-        const data = await axios.post(sendMessage_URL,{chat_id: "339121864", text: request}).catch(error => console.log(error));
+        const data = await axios.post(sendMessage_URL,{chat_id: "339121864", text: JSON.stringify(request.body)}).catch(error => console.log(error));
         
         response.status(200).json({body: request.body});
     }
