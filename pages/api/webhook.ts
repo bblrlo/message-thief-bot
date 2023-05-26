6169ecc9d8f88e70299e8643d5011028368f7cfa
Pavel Marchenko <bblrlo@gmail.com>
2023-05-26 13:42:01 +0300

    upd

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,10 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import {TelegramBot} from 'node-telegram-bot-api'
 
-export default function webhook(request: NextApiRequest, response: NextApiResponse){
-    response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-        env: process.env.BOT_TOKEN,
-    })
+export default async function webhook(request: NextApiRequest, response: NextApiResponse){
+    const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+    const { chat: { id }, text } = request.body.message;
+    const BODY = JSON.stringify({chat_id: id, text: text});
+    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
+    await bot.sendMessage(id,BODY);
+    console.log(BODY);
+    response.status(200);
 }
