b1c59a7a99a38a1b149ae324b1d7f9b9fd728740
Pavel Marchenko <bblrlo@gmail.com>
2023-05-26 21:08:37 +0300

    upd

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { PrismaClient } from '@prisma/client';
 
 export default async function webhook(request: NextApiRequest, response: NextApiResponse){
+    if (request.method !== 'POST') { return response.status(405)}
     const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
     const sendMessage_URL = `${BASE_URL}/sendMessage`;
     const prisma = new PrismaClient();
@@ -10,7 +11,7 @@ export default async function webhook(request: NextApiRequest, response: NextApi
         const {id: chat_id, type: chat_type} = request.body.result.chat;
         const message = request.body.result.text;
         const {username: sender } = request.body.result.from;
-        const user = await prisma.treasure.create({
+        const new_treasure = await prisma.treasure.create({
             data: {
                 chat_id: chat_id,
                 chat_type: chat_type,
@@ -18,11 +19,7 @@ export default async function webhook(request: NextApiRequest, response: NextApi
                 message: message
             }
         });
-        const {data} = await axios.post(sendMessage_URL,
-            {chat_id: chat_id , text: message}, 
-            {headers: {'Content-Type': 'application/json'}});
-            
-        response.status(200);
+    response.status(200).json(new_treasure.id);
     }catch(error){
         
         response.status(200).json({body: request.body, error: error});
