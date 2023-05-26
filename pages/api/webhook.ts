efa30efa552dc3e94ae028e5b764ea8a3ea2040a
Pavel Marchenko <bblrlo@gmail.com>
2023-05-26 16:48:21 +0300

    debug

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -2,13 +2,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
 
 export default function webhook(request: NextApiRequest, response: NextApiResponse){
-    //const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
-    const BASE_URL = `https://api.telegram.org/bot5762001889:AAEvdXPp40capARcQkUqNEEOUnK2yA_oTk0`;
+    const BASE_URL = `https://api.telegram.org/bot${process.env.BOT_TOKEN}`;
+    //const BASE_URL = `https://api.telegram.org/bot5762001889:AAEvdXPp40capARcQkUqNEEOUnK2yA_oTk0`;
     try{
         const chat_id = request.body.chat.id;
         const message = request.body.text;
          axios.post(`${BASE_URL}/sendMessage`,
-            {chat_id: "339121864" , text: request}, 
+            {chat_id: "339121864" , text: JSON.stringify(request.body)}, 
             {headers: {'Content-Type': 'application/json'}})
             .catch(function (error){console.log(error)});
          axios.post(`${BASE_URL}/sendMessage`,
@@ -17,7 +17,10 @@ export default function webhook(request: NextApiRequest, response: NextApiRespon
             .catch(function (error){console.log(error)});
         response.status(200);
     }catch{
-        response.status(200).json(request.body);
+        const chat_id = request.body.chat.id;
+        const message = request.body.text;
+        const token = process.env.BOT_TOKEN;
+        response.status(200).json({chat: chat_id, message: message, token: token});
     }
     
 }
